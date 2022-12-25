21f200f955c3c7ad7c1dda2e3d82432508af81ef
ayoubdkhissi <1999dkhissi@gmail.com>
2022-12-25 15:30:54 +0100

    Update API KEY

--- SocialBrothers.APIcase.Domain/Common/Constants.cs
+++ SocialBrothers.APIcase.Domain/Common/Constants.cs
@@ -7,5 +7,7 @@ public static class Constants
 {
     public const string DISTANCES_API_BASE_URL = "https://api.distancematrix.ai/maps/api/distancematrix/json";
 
-    public const string DISTANCES_API_KEY = "T8wD5mslbPHPc6QoEL9LsIVx7xA7O";
+    public const string DISTANCES_API_KEY = "DO2TAdqmmr0QHRiFbR0JyHfSLG6WK";
+
+    // old key = "T8wD5mslbPHPc6QoEL9LsIVx7xA7O"
 }
